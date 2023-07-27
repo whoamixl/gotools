c91984d4262dfe56de6b83e020fa5d898ab8f6c9
WHOAMI <2387135321@qq.com>
2023-07-28 00:24:28 +0800

    fix: bug

--- main.go
+++ main.go
@@ -10,4 +10,6 @@ func main() {
 	fmt.Println("qweerrt"[:])
 	fmt.Println(DesensitizedUtil.ChineseName("闫小龙"))
 	fmt.Println(DesensitizedUtil.IdCard("5137211998091", 1, 2))
+	fmt.Println(DesensitizedUtil.FixedPhone("09157518479x"))
+	fmt.Println(DesensitizedUtil.MobilePhone("18899999923"))
 }
--- utils/DesensitizedUtil/DesensitizedUtil.go
+++ utils/DesensitizedUtil/DesensitizedUtil.go
@@ -2,13 +2,17 @@ package DesensitizedUtil
 
 import (
 	"errors"
+	"github.com/whoamixl/gotools/utils/NumberUtil"
+	"github.com/whoamixl/gotools/utils/SliceUtil"
 )
 
+// UserId function takes a string parameter and returns a hidden version of the string.
 func UserId(str string) string {
 	data, _ := hide(str, 1, 0)
 	return data
 }
 
+// ChineseName function takes a string parameter and returns a hidden version of the string, based on the length of the string.
 func ChineseName(str string) string {
 	if len(str) == 2 {
 		data, _ := hide(str, 0, 1)
@@ -21,14 +25,35 @@ func ChineseName(str string) string {
 	return str
 }
 
+// IdCard function takes a string parameter and two integer parameters, start and end, and returns a hidden version of the string based on the specified start and end positions.
 func IdCard(str string, start, end int) (string, error) {
-	if len(str) != 18 {
-		return str, errors.New("The card number is not legal")
+	if len(str) == 18 {
+		data, err := hide(str, start, end)
+		return data, err
 	}
-	data, err := hide(str, start, end)
-	return data, err
+	return str, errors.New("The card number is not legal")
 }
 
+func FixedPhone(str string) (string, error) {
+	length := len(str)
+	lengthSlice := []int{10, 11, 12}
+	if SliceUtil.Contains(lengthSlice, length) && NumberUtil.IsNumber(str) {
+		data, _ := hide(str, 4, 2)
+		return data, nil
+	}
+	return str, errors.New("The fixed phone number is legal")
+
+}
+
+func MobilePhone(str string) (string, error) {
+	if len(str) == 11 && NumberUtil.IsNumber(str) || str[:1] == "1" {
+		data, _ := hide(str, 3, 4)
+		return data, nil
+	}
+	return str, errors.New("The fixed phone number is legal")
+}
+
+// hide function is a generic function for hiding strings. It takes a string parameter and two integer parameters, start and end, and returns a hidden version of the string based on the specified start and end positions.
 func hide(str string, start, end int) (string, error) {
 	if (start + end) >= len(str) {
 		return str, errors.New("The length of the start and end cannot be greater than the length of str")
@@ -44,6 +69,7 @@ func hide(str string, start, end int) (string, error) {
 	return str[:start] + generateAsterisk(len(str)-start-end) + str[len(str)-end:], nil
 }
 
+// generateAsterisk function takes an integer parameter and returns a string of asterisks based on the specified number.
 func generateAsterisk(num int) string {
 	strS := ""
 	for i := 0; i < num; i++ {
